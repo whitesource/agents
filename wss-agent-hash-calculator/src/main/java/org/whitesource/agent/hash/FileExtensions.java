c67db72f408effc95748820f53bda19c51a9b09f
ChenLuigi <chen.luigi@whitesourcesoftware.com>
2018-01-29 11:27:19 +0200

    WSE-41 Remove the hardcoded extensions in docker agent and add all the new extensions to FileExtensions in agent

--- wss-agent-hash-calculator/src/main/java/org/whitesource/agent/hash/FileExtensions.java
+++ wss-agent-hash-calculator/src/main/java/org/whitesource/agent/hash/FileExtensions.java
@@ -1,9 +1,6 @@
 package org.whitesource.agent.hash;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Iterator;
+import java.util.*;
 
 /**
  * Created by anna.rozin
@@ -16,27 +13,64 @@ public class FileExtensions {
 
     private static final String OR_REGEX = "|";
 
+    public static final String GLOB_PATTERN_PREFIX = "**/*.";
+
     public static final String BINARY_FILE_EXTENSION_REGEX;
+
+    public static final List<String> SOURCE_EXTENSIONS = Arrays.asList(
+            "c", "cc", "cp", "cpp", "cxx", "c\\+\\+", "h", "hh", "hpp", "hxx", "h\\+\\+", "m", "mm", "pch", // C and C++
+            "c#", "cs", "csharp",  // C#
+            "go", "goc",  // GO
+            "js", // JavaScript
+            "pl", "plx", "pm", "ph", "cgi", "fcgi", "pod", "psgi", "al", "perl", "t", // PERL
+            "pl6", "p6m", "p6l", "pm6", "nqp", "6pl", "6pm", "p6", // PERL6
+            "php", // PHP
+            "py", // Python
+            "rb", // Ruby
+            "swift", // Swift
+            "java", // Java
+            "clj", "cljx", "cljs", "cljc"); // Clojure
 
     private static final Collection<String> GENERIC_RESOURCE_FILE_EXTENSIONS = new ArrayList<>(
             Arrays.asList("air", "apk", "bz2", "deb", "drpm", "dmg", "egg", "exe",
                     "gem", "gzip", "jar", "msi", "nupkg", "rpm", "swc", "swf",
-                    "tar.bz2", "tar.gz", "tgz", "pkg.tar.xz", "udeb", "whl", "zip"));
+                    "tar.bz2", "tar.gz", "tar", "tgz", "pkg.tar.xz", "udeb", "whl", "zip", "(u)?deb", "(a)?rpm"));
 
-    private static final Collection<String> JAVA_FILE_EXTENSIONS = new ArrayList<>(
+    /*private static final Collection<String> JAVA_FILE_EXTENSIONS = new ArrayList<>(
             Arrays.asList("jar", "war", "aar", "ear", "car"));
 
+    //todo merge between JAVA_FILE_EXTENSIONS and ARCHIVE_EXTENSIONS
+    public static final List<String> ARCHIVE_EXTENSIONS = Arrays.asList("war", "ear", "zip", "whl", "tar.gz", "tgz", "tar");*/
+    public static final List<String> ARCHIVE_EXTENSIONS = Arrays.asList("war", "ear", "zip", "whl", "tar.gz", "tgz", "tar", "car", "aar", "jar");
     private static final Collection<String> DOT_NET_FILE_EXTENSIONS = new ArrayList<>(
             Arrays.asList("dll", "exe"));
+    public static final String SOURCE_FILE_PATTERN;
+    public static final String BINARY_FILE_PATTERN;
+    public static final String ARCHIVE_FILE_PATTERN;
+    public static final String[] INCLUDES;
+    public static final String[] EXCLUDES = new String[]{"**/*sources.jar", "**/*javadoc.jar", "**/tests/**"};
+    public static final String[] ARCHIVE_INCLUDES;
+    public static final String[] ARCHIVE_EXCLUDES = new String[]{"**/*sources.jar", "**/*javadoc.jar", "**/tests/**"};
+
 
     static {
+        SOURCE_FILE_PATTERN = buildRegexString(SOURCE_EXTENSIONS);
+        BINARY_FILE_PATTERN = buildRegexString(GENERIC_RESOURCE_FILE_EXTENSIONS);
+        ARCHIVE_FILE_PATTERN = buildRegexString(ARCHIVE_EXTENSIONS);
+
         StringBuilder sb = new StringBuilder();
-        sb.append(buildRegexString(GENERIC_RESOURCE_FILE_EXTENSIONS));
+        sb.append(BINARY_FILE_PATTERN);
         sb.append(OR_REGEX);
-        sb.append(buildRegexString(JAVA_FILE_EXTENSIONS));
+        sb.append(ARCHIVE_FILE_PATTERN);
         sb.append(OR_REGEX);
         sb.append(buildRegexString(DOT_NET_FILE_EXTENSIONS));
         BINARY_FILE_EXTENSION_REGEX = sb.toString();
+
+        List<String> allExtensions = new ArrayList<>();
+        allExtensions.addAll(SOURCE_EXTENSIONS);
+        allExtensions.addAll(GENERIC_RESOURCE_FILE_EXTENSIONS);
+        INCLUDES = initializeGlobPattern(allExtensions);
+        ARCHIVE_INCLUDES = initializeGlobPattern(ARCHIVE_EXTENSIONS);
     }
 
     /* --- Private methods --- */
@@ -53,6 +87,14 @@ public class FileExtensions {
             }
         }
         return sb.toString();
+    }
+
+    private static String[] initializeGlobPattern(List<String> extensions) {
+        String[] globPatterns = new String[extensions.size()];
+        for (int i = 0; i < extensions.size(); i++) {
+            globPatterns[i] = GLOB_PATTERN_PREFIX + extensions.get(i);
+        }
+        return globPatterns;
     }
 
 }
