497349036d07da27f28c53f1f45c4ea60d88b1bb
ChenLuigi <chen.luigi@whitesourcesoftware.com>
2018-09-17 11:49:42 +0300

    Create new class called vulnerability entity

---
+++ wss-agent-api/src/main/java/org/whitesource/agent/api/model/VulnerableEntity.java
@@ -0,0 +1,45 @@
+package org.whitesource.agent.api.model;
+
+import java.io.Serializable;
+
+/**
+ * @author chen.luigi
+ */
+public class VulnerableEntity implements Serializable {
+
+    private static final long serialVersionUID = 6521642112001095631L;
+
+    private String element;
+    private int startLine;
+    private int endLine;
+
+    public VulnerableEntity(String element, int startLine, int endLine) {
+        this.element = element;
+        this.startLine = startLine;
+        this.endLine = endLine;
+    }
+
+    public String getElement() {
+        return element;
+    }
+
+    public void setElement(String element) {
+        this.element = element;
+    }
+
+    public int getStartLine() {
+        return startLine;
+    }
+
+    public void setStartLine(int startLine) {
+        this.startLine = startLine;
+    }
+
+    public int getEndLine() {
+        return endLine;
+    }
+
+    public void setEndLine(int endLine) {
+        this.endLine = endLine;
+    }
+}
