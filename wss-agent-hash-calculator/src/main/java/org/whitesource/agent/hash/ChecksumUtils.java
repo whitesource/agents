5d4a6f8ac026b6103765ce4b08ee3351620b0dee
ChenLuigi <chen.luigi@whitesourcesoftware.com>
2019-12-24 11:27:53 +0200

    WSE-3214 Add condition to calculate super hash in case of "++" extension

--- wss-agent-hash-calculator/src/main/java/org/whitesource/agent/hash/ChecksumUtils.java
+++ wss-agent-hash-calculator/src/main/java/org/whitesource/agent/hash/ChecksumUtils.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2012 White Source Ltd.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -108,7 +108,7 @@ public final class ChecksumUtils {
         StringBuilder superHash = new StringBuilder("");
         HashCalculator superHashCalculator = new HashCalculator();
         if (dependencyFile.getName().toLowerCase().matches(FileExtensions.SOURCE_FILE_PATTERN) ||
-                dependencyFile.getName().toLowerCase().endsWith("c++") || dependencyFile.getName().toLowerCase().endsWith("h++")) {
+                dependencyFile.getName().toLowerCase().endsWith("++")) {
             try {
                 HashCalculationResult superHashResult = superHashCalculator.calculateSuperHash(dependencyFile);
                 if (superHashResult != null) {
