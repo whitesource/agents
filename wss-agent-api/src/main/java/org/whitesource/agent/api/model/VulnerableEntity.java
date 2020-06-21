9b2f4c67e329211c641e83bc152f6522b2df94dc
AnnaRozin <anna.rozin@whitesourcesoftware.com>
2020-06-21 11:07:13 +0300

    WSE-4359 Update VulnerableEntity class

--- wss-agent-api/src/main/java/org/whitesource/agent/api/model/VulnerableEntity.java
+++ wss-agent-api/src/main/java/org/whitesource/agent/api/model/VulnerableEntity.java
@@ -17,12 +17,31 @@ public class VulnerableEntity implements Serializable {
     private int startLine;
     private int endLine;
 
+    //since 2.9.9.51
+    private String namespace;
+    private String className;
+    private String method;
+    private String language;
+    private String type;
+    private String extraData;
+
     /* --- Constructors --- */
 
     public VulnerableEntity(String element, int startLine, int endLine) {
         this.element = element;
         this.startLine = startLine;
         this.endLine = endLine;
+    }
+
+    public VulnerableEntity(String element, int startLine, int endLine, String namespace, String className,
+                            String method, String language, String type, String extraData) {
+        this(element, startLine, endLine);
+        this.namespace = namespace;
+        this.className = className;
+        this.method = method;
+        this.language = language;
+        this.type = type;
+        this.extraData = extraData;
     }
 
     /* --- Getters / Setters --- */
@@ -49,5 +68,53 @@ public class VulnerableEntity implements Serializable {
 
     public void setEndLine(int endLine) {
         this.endLine = endLine;
+    }
+
+    public String getNamespace() {
+        return namespace;
+    }
+
+    public void setNamespace(String namespace) {
+        this.namespace = namespace;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getExtraData() {
+        return extraData;
+    }
+
+    public void setExtraData(String extraData) {
+        this.extraData = extraData;
     }
 }
