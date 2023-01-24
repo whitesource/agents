b605915f1994d09eaf5ad7011b5ea4c47a26ace9
dor-hayun <dor.hayun@whitesourcesoftware.com>
2023-01-24 16:28:30 +0200

    Adding setters and getters

--- wss-agent-api/src/main/java/org/whitesource/agent/api/model/Code.java
+++ wss-agent-api/src/main/java/org/whitesource/agent/api/model/Code.java
@@ -4,5 +4,11 @@ import java.util.List;
 
 public class Code {
     private List<Line> lines;
+    public List<Line> getLines() {
+        return lines;
+    }
+    public void setLines(List<Line> lines) {
+        this.lines = lines;
+    }
 
 }
--- wss-agent-api/src/main/java/org/whitesource/agent/api/model/Line.java
+++ wss-agent-api/src/main/java/org/whitesource/agent/api/model/Line.java
@@ -11,4 +11,70 @@ public class Line {
     private boolean firstCause;
     private boolean lastCause;
 
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public boolean isCause() {
+        return isCause;
+    }
+
+    public void setCause(boolean cause) {
+        isCause = cause;
+    }
+
+    public String getAnnotation() {
+        return annotation;
+    }
+
+    public void setAnnotation(String annotation) {
+        this.annotation = annotation;
+    }
+
+    public boolean isTruncated() {
+        return truncated;
+    }
+
+    public void setTruncated(boolean truncated) {
+        this.truncated = truncated;
+    }
+
+    public String getHighlighted() {
+        return highlighted;
+    }
+
+    public void setHighlighted(String highlighted) {
+        this.highlighted = highlighted;
+    }
+
+    public boolean isFirstCause() {
+        return firstCause;
+    }
+
+    public void setFirstCause(boolean firstCause) {
+        this.firstCause = firstCause;
+    }
+
+    public boolean isLastCause() {
+        return lastCause;
+    }
+
+    public void setLastCause(boolean lastCause) {
+        this.lastCause = lastCause;
+    }
+
+
+
 }
