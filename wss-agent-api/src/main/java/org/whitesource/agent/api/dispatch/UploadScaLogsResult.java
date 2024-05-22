77a973afa4faa70883bf38a3078df0fd5ee8d3b9
Alex Maybaum <alex.maybaum@whitesourcesoftware.com>
2024-05-22 09:40:38 +0300

    WEI-3783 add success and message fields in UploadScaLogsResult

--- wss-agent-api/src/main/java/org/whitesource/agent/api/dispatch/UploadScaLogsResult.java
+++ wss-agent-api/src/main/java/org/whitesource/agent/api/dispatch/UploadScaLogsResult.java
@@ -2,15 +2,27 @@ package org.whitesource.agent.api.dispatch;
 
 public class UploadScaLogsResult extends BaseResult {
 
-    /* --- Static members --- */
     private static final long serialVersionUID = -8474225446757628208L;
-    /* --- Constructors ---*/
+    private boolean success;
+    private String message;
 
-    /***
-     * default constructor
-     */
     public UploadScaLogsResult() {
         super();
     }
 
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
