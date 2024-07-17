649bf7ae763f0d7968b11b9cfcb2ab95d601126a
romansoko <69453555+romansoko@users.noreply.github.com>
2024-07-17 16:25:31 +0300

    Update SendMetricsResult.java

--- wss-agent-api/src/main/java/org/whitesource/agent/api/dispatch/SendMetricsResult.java
+++ wss-agent-api/src/main/java/org/whitesource/agent/api/dispatch/SendMetricsResult.java
@@ -1,6 +1,5 @@
 package org.whitesource.agent.api.dispatch;
 
-import java.io.Serial;
 
 public class SendMetricsResult extends BaseResult {
     private static final long serialVersionUID = 7986011495688324618L;
